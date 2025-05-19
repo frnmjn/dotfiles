3de71ff2a9f1440225c47846fa7a071b502e71b1
Emilio Junior Francischetti <ejfrancischetti@gmail.com>
2025-05-19 21:56:05 +0200

    Add delete word in wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -20,8 +20,8 @@ config.window_decorations = 'RESIZE'
 
 
 -- needed for symbol keys
-config.send_composed_key_when_left_alt_is_pressed = true
-config.send_composed_key_when_right_alt_is_pressed = true
+config.send_composed_key_when_left_alt_is_pressed = false
+config.send_composed_key_when_right_alt_is_pressed = false
 config.use_dead_keys = false
 config.use_ime = false
 
@@ -32,20 +32,20 @@ config.keys = {
   --{ key = 'j',          mods = 'CMD',     action = act.SplitVertical { domain = 'CurrentPaneDomain', }, },
   { key = 'LeftArrow',  mods = 'CMD',     action = act.SendKey { key = 'Home', }, },
   { key = 'RightArrow', mods = 'CMD',     action = act.SendKey { key = 'End', }, },
+  { key = '+',          mods = 'CMD',     action = act.SendKey { key = '*', mods = 'CMD' }, },
+  { key = '+',          mods = 'CMD',     action = act.IncreaseFontSize },
+  { key = '-',          mods = 'CMD',     action = act.DecreaseFontSize },
+  { key = '0',          mods = 'CMD',     action = act.ResetFontSize },
   --{ key = 'h',          mods = 'CTRL',    action = act.ActivatePaneDirection 'Left', },
   --{ key = 'k',          mods = 'CTRL',    action = act.ActivatePaneDirection 'Up', },
   --{ key = 'j',          mods = 'CTRL', 	  action = act.ActivatePaneDirection 'Down', },
   { key = 'w',          mods = 'CTRL',    action = act.SendKey { key = 'f', mods = 'ALT' }, },
   { key = 'b',          mods = 'CTRL',    action = act.SendKey { key = 'b', mods = 'ALT' }, },
+  { key = 'k',          mods = 'CTRL',    action = act.ScrollByLine(-10) },
+  { key = 'j',          mods = 'CTRL',    action = act.ScrollByLine(10) },
   { key = 'd',          mods = 'ALT',     action = act.SendKey { key = 'u', mods = 'CTRL' }, },
-  { key = 'l',          mods = 'ALT',       action = act.SendKey { key = 'l', mods = 'CTRL'}, },
-  { key = 'k',          mods = 'CTRL', action = act.ScrollByLine(-10) },
-  { key = 'j',          mods = 'CTRL', action = act.ScrollByLine(10) },
-  { key = '+',          mods = 'CMD',    action = act.SendKey { key = '*', mods = 'CMD' }, },
-  --{ key = 'Backspace',  mods = 'ALT',    action = act.SendKey { key = 'Backspace', mods = 'ALT' }, },
-  { key = '+',          mods = 'CMD', action = act.IncreaseFontSize },
-  { key = '-',          mods = 'CMD', action = act.DecreaseFontSize },
-  { key = '0',          mods = 'CMD', action = act.ResetFontSize },
+  { key = 'l',          mods = 'ALT',     action = act.SendKey { key = 'l', mods = 'CTRL'}, },
+  { key = 'Backspace',  mods = 'ALT',     action = act.SendKey {key = 'w', mods = 'CTRL'} },
 }
 
 return config
