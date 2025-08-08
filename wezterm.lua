cf0d27e15d5af162c74984791013927fe315c0f1
Emilio Junior Francischetti <ejfrancischetti@gmail.com>
2025-08-08 13:02:10 +0200

    Vscode  and wezterm adjustment

--- wezterm.lua
+++ wezterm.lua
@@ -19,7 +19,6 @@ config.color_scheme = 'AdventureTime'
 config.window_decorations = 'RESIZE'
 
 
--- needed for symbol keys
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = false
 config.use_dead_keys = false
@@ -28,24 +27,22 @@ config.use_ime = false
 -- config.disable_default_key_bindings = true
 
 config.keys = {
-  --{ key = 'l',          mods = 'CMD',     action = act.SplitHorizontal { domain = 'CurrentPaneDomain', }, }, 
-  --{ key = 'j',          mods = 'CMD',     action = act.SplitVertical { domain = 'CurrentPaneDomain', }, },
   { key = 'LeftArrow',  mods = 'CMD',     action = act.SendKey { key = 'Home', }, },
   { key = 'RightArrow', mods = 'CMD',     action = act.SendKey { key = 'End', }, },
-  { key = '+',          mods = 'CMD',     action = act.SendKey { key = '*', mods = 'CMD' }, },
   { key = '+',          mods = 'CMD',     action = act.IncreaseFontSize },
   { key = '-',          mods = 'CMD',     action = act.DecreaseFontSize },
   { key = '0',          mods = 'CMD',     action = act.ResetFontSize },
-  --{ key = 'h',          mods = 'CTRL',    action = act.ActivatePaneDirection 'Left', },
-  --{ key = 'k',          mods = 'CTRL',    action = act.ActivatePaneDirection 'Up', },
-  --{ key = 'j',          mods = 'CTRL', 	  action = act.ActivatePaneDirection 'Down', },
-  { key = 'w',          mods = 'CTRL',    action = act.SendKey { key = 'f', mods = 'ALT' }, },
-  { key = 'b',          mods = 'CTRL',    action = act.SendKey { key = 'b', mods = 'ALT' }, },
+  -- { key = 'w',          mods = 'CTRL',    action = act.SendKey { key = 'f', mods = 'ALT' }, },
+  -- { key = 'b',          mods = 'CTRL',    action = act.SendKey { key = 'b', mods = 'ALT' }, },
   { key = 'PageUp',                       action = act.ScrollByLine(-10) },
   { key = 'PageDown',                     action = act.ScrollByLine(10) },
-  { key = 'd',          mods = 'ALT',     action = act.SendKey { key = 'u', mods = 'CTRL' }, },
-  { key = 'l',          mods = 'ALT',     action = act.SendKey { key = 'l', mods = 'CTRL'}, },
-  { key = 'Backspace',  mods = 'ALT',     action = act.SendKey {key = 'w', mods = 'CTRL'} },
+  -- { key = 'Escape',               action = act.SendKey { key = 'u', mods = 'CTRL' }, },
+  --  Clear
+  -- { key = 'l',          mods = 'ALT',     action = act.SendKey { key = 'l', mods = 'CTRL'}, },
+  -- Delete Left Word
+  { key = 'h',  mods = 'ALT',     action = act.SendKey {key = 'w', mods = 'CTRL'} },
+  -- Delete Right Word
+  { key = 'l',  mods = 'ALT',     action = act.SendKey {key = 'd', mods = 'ALT'} },
 }
 
 return config
