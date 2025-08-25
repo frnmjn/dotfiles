d0c468edf7f532bb6e2af7217794af438462219b
Emilio Junior Francischetti <ejfrancischetti@gmail.com>
2025-08-25 15:53:25 +0200

    wezterm clean up

--- wezterm.lua
+++ wezterm.lua
@@ -24,21 +24,14 @@ config.send_composed_key_when_right_alt_is_pressed = false
 config.use_dead_keys = false
 config.use_ime = false
 
--- config.disable_default_key_bindings = true
-
 config.keys = {
   { key = 'LeftArrow',  mods = 'CMD',     action = act.SendKey { key = 'Home', }, },
   { key = 'RightArrow', mods = 'CMD',     action = act.SendKey { key = 'End', }, },
   { key = '+',          mods = 'CMD',     action = act.IncreaseFontSize },
   { key = '-',          mods = 'CMD',     action = act.DecreaseFontSize },
   { key = '0',          mods = 'CMD',     action = act.ResetFontSize },
-  -- { key = 'w',          mods = 'CTRL',    action = act.SendKey { key = 'f', mods = 'ALT' }, },
-  -- { key = 'b',          mods = 'CTRL',    action = act.SendKey { key = 'b', mods = 'ALT' }, },
   { key = 'PageUp',                       action = act.ScrollByLine(-10) },
   { key = 'PageDown',                     action = act.ScrollByLine(10) },
-  -- { key = 'Escape',               action = act.SendKey { key = 'u', mods = 'CTRL' }, },
-  --  Clear
-  -- { key = 'l',          mods = 'ALT',     action = act.SendKey { key = 'l', mods = 'CTRL'}, },
   -- Delete Left Word
   { key = 'h',  mods = 'ALT',     action = act.SendKey {key = 'w', mods = 'CTRL'} },
   -- Delete Right Word
